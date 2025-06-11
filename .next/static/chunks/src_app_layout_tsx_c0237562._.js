97d289a5051bb4b0cba387208a8f9a7251b134ad
nursapri <nursafripanda@gmail.com>
2025-06-11 18:26:46 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749641206217

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_fc0e1c1c._.js",
+  "static/chunks/src_07e8aeeb._.js"
+],
+    source: "dynamic"
+});
